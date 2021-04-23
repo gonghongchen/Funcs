f56b54b9ee5886fd5c5a37f2e7fdc959f746775f
gonghongchen <gonghongchen@hikvision.com>
2021-04-23 21:37:12 +0800

    update: isEmpty

--- isEmpty.js
+++ isEmpty.js
@@ -2,3 +2,6 @@ const isEmpty = val => {
   if (val instanceof Object) return Object.values(val).length === 0  // includes Map, Set, Object, Array, arguments
   return !val  // includes String, Number, undefined, null, Boolean
 }
+
+// MDN - instanceof 
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/instanceof
