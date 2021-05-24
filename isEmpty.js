98f47a875ab8df9f17b9213a0cc035941a54394c
gonghongchen <1257075652@qq.com>
2021-05-24 15:24:12 +0800

    Update isEmpty.js

--- isEmpty.js
+++ isEmpty.js
@@ -3,5 +3,7 @@ const isEmpty = val => {
   return !val  // includes String, Number, undefined, null, Boolean
 }
 
+// TODO 对于 isEmpty(Object.create(null)) 会返回 false，因为使用 Object.create(null) 创建的对象没有原型对象，所以无法使用 instanceof 进行准确判断
+
 // MDN - instanceof 
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/instanceof
