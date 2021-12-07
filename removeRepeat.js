c631436c4c93581308b05159a49c5462edec9e8e
gonghongchen <1257075652@qq.com>
2021-12-07 14:28:59 +0800

    update removeMultiRepeat

--- removeRepeat.js
+++ removeRepeat.js
@@ -19,21 +19,21 @@ const removeRepeat = arr => {
 // ===========================================================================
 
 // 多维数组去重
-
-const removeRepeat = arr => {
-    const deep = 2
-    // [1, [2, 3], [2, [4, [5]]]]
-    // 获取数组的最大深度
-    const getDeep = arr => {
-        const totalDeep = 1
-        const curDeep = 1
+const removeMultiRepeat = arr => {
+    // 将多维数组转换为一维数组
+    const flat = arr => {
+        let newArr = []
         arr.forEach(item => {
             if (Array.isArray(item)) {
-                curDeep++
-                totalDeep += getDeep(item)
+                newArr = newArr.concat(flat(item))
+            } else {
+                newArr.push(item)
             }
         })
-        return totalDeep
+        return newArr
     }
-    return [...new Set(arr.flat(deep))] // flat from ES2019
+
+    return [...new Set(flat(arr))] //  也可以改用上面的 removeRepeat 方法
 }
+
+removeMultiRepeat([1, [2, 3], [2, [8, 9, 1], [4, [5, 8]]]]) //  [1, 2, 3, 8, 9, 4, 5]
