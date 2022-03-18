b0f10d80759f80a02e7ca736a1aa875a519fcb80
H&C <1257075652@qq.com>
2022-03-18 17:18:11 +0800

    Update getQueryByName.js

--- getQueryByName.js
+++ getQueryByName.js
@@ -3,5 +3,12 @@ const getQueryByName = name => {
     const queryList = location.href.split('?')[1]?.split('&') || []
     const curQuery = queryList.filter(item => item.includes(name))[0]
 
+    // history mode
+    // new URL(location.href).searchParams.get(name)
+    // 或 new URLSearchParams(location.search).get(name)
+    
+    // hash mode
+    // new URLSearchParams(location.hash.split('?')[1]).get(name)
+    
     return curQuery?.split('=')[1] || ''
 }
